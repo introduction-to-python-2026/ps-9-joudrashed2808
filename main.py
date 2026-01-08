58ed17857ce8cd13b8746d2fdf3e417c52f82551
Joud Rashed <joudrashed2808@gmail.com>
2026-01-08 18:41:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import joblib
 
@@ -17,7 +17,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
 
-model = LogisticRegression()
+model = SVC(kernel="rbf", C=20, gamma="scale")
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
